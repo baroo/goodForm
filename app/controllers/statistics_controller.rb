a009c6e89b75eb39e874f53d9769b6bd18db6258
baroo <baroo_the_cat@yahoo.co.uk>
2013-04-18 22:43:19 +0100

    The Palace of Statistice, Reversed

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,7 +2,7 @@ class StatisticsController < ApplicationController
 	def index
 			#param1 = params[:goodForm_id]
 			#render :text => param1.inspect	
-			@statistics = Statistic.where("game_id = ?", params[:goodForm_id]) #Returns an array)
+			@statistics = Statistic.where("game_id = ?", params[:goodForm_id]).reverse_order #Returns an array)
 			@game = Game.where("id = ?", @statistics.first.game_id)
 			@team1 = @game.first.team1.name
 			@team2 = @game.first.team2.name
