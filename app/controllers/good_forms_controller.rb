baa72b1cb955035803140dcd18fb18364095a0ed
baroo <baroo_the_cat@yahoo.co.uk>
2013-04-20 16:27:51 +0100

    sorted games DESC

--- app/controllers/good_forms_controller.rb
+++ app/controllers/good_forms_controller.rb
@@ -2,13 +2,20 @@ class GoodFormsController < ApplicationController
 
 	def index
 		@teams = Team.search(params[:search])
+		@statsRow = Statistic.find(:first, :order => 'hg DESC') #Get the whole Row with the largest HG value
+		@maxHg = @statsRow.hg
+		@ag = @statsRow.ag
+		#@game = Game.where("game.id = ?", 1)
+		#@team1 = @game.first.team1.name
+		
+		
 	end
 	
 	def show
 		@team        = Team.find(params[:id])
 		#@league     = League.find()
-		@games       = Game.where("team1 = ? OR team2 = ?", @team.id, @team.id) #Returns an array
-		#@statistics = @game.first.statistics
+		@games       = Game.where("team1 = ? OR team2 = ?", @team.id, @team.id) #Returns an array 
+		@gamesSorted = @games.all(:order => "kodate DESC")
 		#@statistics = Statistics.find_by_gameid(@game.first.hometeamid) # find the statistics for the first game.
 	end
 	
