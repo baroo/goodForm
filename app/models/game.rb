f963bd9d423c67552172b09a5e5aec3b9ac44e89
baroo <baroo_the_cat@yahoo.co.uk>
2013-05-16 21:30:27 +0100

    search stuff

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,11 +1,12 @@
 class Game < ActiveRecord::Base
   attr_accessible :league_id, :kodate, :team1, :team2
+  
   belongs_to :league
   has_one :statistic
   has_one :team1, :class_name => "Team", :primary_key =>"team1", :foreign_key => "id"
   has_one :team2, :class_name => "Team", :primary_key =>"team2", :foreign_key => "id"
   
    def self.search()
-		where(:kodate => (Time.now - 1.day)..Time.now )
+		where(:kodate => (Time.now - 2.hours)..Time.now )
 	end
 end
