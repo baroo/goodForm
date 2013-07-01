47cace45b07dc3565c49adb9897a24c88c8bf119
baroo <baroo_the_cat@yahoo.co.uk>
2013-07-01 23:19:10 +0100

    average corners added to games summary

--- app/controllers/good_forms_controller.rb
+++ app/controllers/good_forms_controller.rb
@@ -16,6 +16,40 @@ class GoodFormsController < ApplicationController
 		@gamesSorted = @games.all(:order => "kodate DESC")
 		#@statistics = Statistics.find_by_gameid(@game.first.team1.id) # find the statistics for the first game.
 		@teamsInSameleague = Team.where("name = ?", @games.first.league.name)
+		
+
+		
+		#Find the statistics for the games for the team with team.id (1) or team.id (2)
+		#Then aggregate the stats and find averages.
+		
+		@totalHomeCorners = 0
+		@totalAwayCorners = 0
+
+		@homeGames = Game.where("team1 = ?", @team.id)
+		
+		@homeGames.each do |hgame|
+			@homeCornerStats = Statistic.where("game_id = ?", hgame.id)
+			
+			if !@homeCornerStats.empty?
+				@totalHomeCorners += @homeCornerStats.last.hco
+			end
+		end
+
+		@awayGames = Game.where("team2 = ?", @team.id)
+		
+		@awayGames.each do |agame|
+			@awayCornerStats = Statistic.where("game_id = ?", agame.id)
+			
+			if !@awayCornerStats.empty?
+				@totalAwayCorners += @awayCornerStats.last.aco
+			end
+		end
+		
+		@totalCorners = @totalHomeCorners+@totalAwayCorners
+		
+		@averageHomeCorners = @totalHomeCorners/@homeGames.size
+		@averageAwayCorners = @totalAwayCorners/@awayGames.size
+			
 	end
 	
 	def new
