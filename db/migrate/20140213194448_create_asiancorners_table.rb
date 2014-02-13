72bed1f164a9b84a9684a488bb497b334b83f899
David <d.m.murray@gmail.com>
2014-02-13 20:40:11 +0000

    int to integer

--- db/migrate/20140213194448_create_asiancorners_table.rb
+++ db/migrate/20140213194448_create_asiancorners_table.rb
@@ -1,7 +1,7 @@
 class CreateAsianCornersTable < ActiveRecord::Migration
   def up
       create_table :asiancorners do |t|
-      t.int :id :default => 0 
+      t.integer :id :default => 0 
       t.string :game_id
       t.string :cornerline
       t.string :homeprice
