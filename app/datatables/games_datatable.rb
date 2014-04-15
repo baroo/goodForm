2dd8af8aa9463addd27cf4d92b21ba79943ece8a
baroo <baroo_the_cat@yahoo.co.uk>
2014-04-15 08:10:49 +0100

    Missing bracket

--- app/datatables/games_datatable.rb
+++ app/datatables/games_datatable.rb
@@ -23,6 +23,7 @@ class GamesDatatable
           link_to(game.team1.name, goodForm_path(game.team1)) ,
           link_to(game.team2.name, goodForm_path(game.team2)),
           link_to_unless(Statistic.where("game_id = ?", game.id).last.nil? "Click for Statistics", goodForm_statistics_path(game))
+        ]
       end
     end
   
