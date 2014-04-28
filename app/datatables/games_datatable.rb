2058b9000ef3bcfc1dc00e6e03691c5904d8f491
baroo <baroo_the_cat@yahoo.co.uk>
2014-04-28 23:10:16 +0100

    hmm

--- app/datatables/games_datatable.rb
+++ app/datatables/games_datatable.rb
@@ -23,7 +23,7 @@ class GamesDatatable
           link_to(game.team1.name, goodForm_path(game.team1)),
           link_to(game.team2.name, goodForm_path(game.team2)),
           link_to("Statistics", goodForm_statistics_path(game)),
-          link_to("Predictions", goodForm_predictions_path(game))
+          #link_to("Predictions", goodForm_predictions_path(game))
           #link_to_unless(Statistic.where("game_id = ?", game.id).last.nil? "Click for Statistics", goodForm_statistics_path(game))
         ]
       end
