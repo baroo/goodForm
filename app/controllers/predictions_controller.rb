d77d2bf4985ddc3533c79157167b22e3df37aac7
baroo <baroo_the_cat@yahoo.co.uk>
2014-05-25 12:45:40 +0100

    Added predictions route, controller and simple view

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -1,4 +1,29 @@
 class PredictionsController < ApplicationController
 	def index
-	end
+	  @predictions = PredictionData.where("gameid = ?", params[:goodForm_id])
+  end
+  
+  def show
+
+  end
+  
+  def new
+  
+  end
+  
+  def create
+  
+  end
+  
+  def edit
+  
+  end
+  
+  def update
+  
+  end
+  
+  def destroy
+  
+  end
 end
--- app/datatables/games_datatable.rb
+++ app/datatables/games_datatable.rb
@@ -1,5 +1,5 @@
 class GamesDatatable
-  delegate :params, :link_to, :goodForm_path, :goodForm_statistics_path, to: :@view
+  delegate :params, :link_to, :goodForm_path, :goodForm_statistics_path, :goodForm_predictions_path, to: :@view
   
   def initialize(view)
       @view = view
@@ -23,7 +23,7 @@ class GamesDatatable
           link_to(game.team1.name, goodForm_path(game.team1)),
           link_to(game.team2.name, goodForm_path(game.team2)),
           link_to("Statistics", goodForm_statistics_path(game)),
-          #link_to("Predictions", goodForm_predictions_path(game))
+          link_to("Predictions", goodForm_predictions_path(game))
           #link_to_unless(Statistic.where("game_id = ?", game.id).last.nil? "Click for Statistics", goodForm_statistics_path(game))
         ]
       end
@@ -36,7 +36,8 @@ class GamesDatatable
    def fetch_games
      games = Game.order("#{sort_column} #{sort_direction}")
      games = games.page(page).per_page(per_page)
-     games.where(:kodate => (Time.current - 2.hours || Time.current + 12.hours)..Time.current )
+     #games.where(:kodate => (Time.current - 2.hours || Time.current + 12.hours)..Time.current )
+     games.where(:kodate => "2014-05-25")
   end    
     
    def page
